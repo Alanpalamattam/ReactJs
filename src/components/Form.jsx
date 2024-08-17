ff610f60f90b61a09bd4ce50731e35d0500a2ccb
Alan Peter <alanpalamattam2@gmail.com>
2024-08-17 13:34:46 +0530

    commit4

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,15 +1,27 @@
 import { useState } from "react";
 
 export default function Form() {
-  const [name, setname] = useState("");
+  const [name, setname] = useState({ firstname: "", lastname: "" });
+  function handleclick(e) {
+    e.preventDefault();
+    console.log(name);
+  }
   return (
     <h2>
+      {name.firstname} - {name.lastname}
       <form>
         <input
-          onChange={(e) => setname(e.target.value)}
+          onChange={(e) => setname({ ...name, firstname: e.target.value })}
           type="text"
-          value={name}
+          value={name.firstname}
         />
+        <input
+          onChange={(e) => setname({ ...name, lastname: e.target.value })}
+          type="text"
+          value={name.lastname}
+        />
+        <br />
+        <button onClick={(e) => handleclick(e)}>Add</button>
       </form>
     </h2>
   );
